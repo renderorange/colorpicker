f6d4a274c281709b84a4a02c2e1061cda1507682
Blaine Motsinger <blaine@renderorange.com>
2024-09-24 08:03:57 -0400

    Remove options and update output

--- main.c
+++ main.c
@@ -29,38 +29,21 @@ int main(int argc, char *argv[]) {
 
 	int opt;
 	int one_shot = 0;
-	int quit_on_keypress = 0;
-	int output_format = 0x11;
 	
 	static struct option long_options[] = {
 		{"one-shot", no_argument, NULL, 'o'},
-		{"quit-on-keypress", no_argument, NULL, 'q'},
-		{"help", no_argument, NULL, 'h'},
-		{"rgb", no_argument, NULL, 'r'},
-		{"hex", no_argument, NULL, 'd'}
+		{"help", no_argument, NULL, 'h'}
 	};
 	
-	while ((opt = getopt_long(argc, argv, "oqhrd", long_options, NULL)) != -1) {
+	while ((opt = getopt_long(argc, argv, "oh", long_options, NULL)) != -1) {
 		switch(opt) {
 			case 'o':
 				one_shot = 1;
 				break;
-			case 'q':
-				quit_on_keypress = 1;
-				break;
-			case 'r':
-				output_format &= 0x1;
-				break;
-			case 'd':
-				output_format &= 0x10;
-				break;
 			case 'h':
 				printf( "colorpicker [options]\n"
-						"  -h, --help:             show this help\n"
-						"  -o, --one-shot:         one shot\n"
-						"  -q, --quit-on-keypress: quit on keypress\n"
-						"  -d, --hex:              hex only\n"
-						"  -r, --rgb:              rgb only\n");
+						"  -h, --help:      display this dialogue\n"
+						"  -o, --one-shot:  read once then exit\n");
 				return 0;
 			default:
 				return 1;
@@ -84,18 +67,12 @@ int main(int argc, char *argv[]) {
 		XImage *image = XGetImage(display, root, 0, 0, gwa.width, gwa.height, AllPlanes, ZPixmap);
 		if (e.type == ButtonPress && e.xbutton.button == Button1) {
 				unsigned long pixel = XGetPixel(image, e.xbutton.x_root, e.xbutton.y_root);
-				if (output_format & 0x1) {
-					printf("%d,%d,%d ", (pixel >> 0x10) & 0xFF, (pixel >> 0x08) & 0xFF, pixel & 0xFF);
-				}
-				if (output_format & 0x10) {
-					printf("#%06X", pixel);
-				}
-				puts("");
+				printf("R: %d, G: %d, B: %d | Hex: #%06X\n", (pixel >> 0x10) & 0xFF, (pixel >> 0x08) & 0xFF, pixel & 0xFF, pixel);
 				if (one_shot) {
 					break;
 				}
 		} else if (e.type == ButtonPress ||
-				(e.type == KeyPress && (e.xkey.keycode == 53 || quit_on_keypress))) {
+				(e.type == KeyPress && e.xkey.keycode == 53)) {
 			break;
 		}
 		XDestroyImage(image);
